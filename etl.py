7a67c0476fff9fe99da732f78677999e7d058805
Noah Silbert <noahpoah@gmail.com>
2020-09-07 11:54:39 -0400

    deleted unused import line

--- etl.py
+++ etl.py
@@ -1,7 +1,6 @@
 import configparser
 from datetime import datetime
 import os
-from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (udf, col, monotonically_increasing_id,
                                    year, month, dayofmonth, hour,
